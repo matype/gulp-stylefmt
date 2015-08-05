e64c0a9dc78dac3b8b632759a628be041576789f
morishitter <morishitter@gmail.com>
2015-08-05 10:51:47 +0900

    implement

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-module.exports = function () {
-}
+var gutil = require('gulp-util');
+var through = require('through2');
+var cssfmt = require('cssfmt');
+
+module.exports = function (options) {
+  options = options || {};
+
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
+
+    if (file.isStream()) {
+      cb(new gutil.PluginError('gulp-cssfmt', 'Streaming not supported'));
+      return;
+    }
+
+    try {
+      file.contents = new Buffer(cssfmt(file.contents.toString()).toString());
+      this.push(file);
+    } catch (err) {
+      this.emit('error', new gutil.PluginError('gulp-cssfmt', err, {fileName: file.path}));
+    }
+
+    cb();
+  });
+};
--- test/index.js
+++ test/index.js
@@ -1,6 +1,20 @@
-var test = require('tape')
-var gulp-cssfmt = require('..')
+var fs = require('fs');
+var assert = require('assert');
+var gutil = require('gulp-util');
+var cssfmt = require('cssfmt');
+var gulpCssfmt = require('../');
 
-test('description', function (t) {
-    t.end()
-})
+it('cssfmt', function (cb) {
+  var stream = gulpCssfmt();
+  var cssFile = fs.readFileSync('test/fixtures/input.css', 'utf-8');
+  var output = cssfmt(cssFile).toString();
+
+  stream.on('data', function (file) {
+    assert.equal(file.contents.toString(), output);
+    cb();
+  });
+
+  stream.write(new gutil.File({
+    contents: new Buffer(cssFile)
+  }));
+});
